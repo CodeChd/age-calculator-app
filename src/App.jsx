8ede182c27b48dd8523b23f6d485352c7706738e
CJ Francisco <Drowzyking1126@gmail.com>
2024-05-04 20:19:27 +0800

    Added date and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,61 @@
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <main className="grid min-h-screen place-content-center w-full h-full">
+
+
+      <div className="p-12 relative bg-neutral-white rounded-2xl rounded-br-[6rem] h-full w-[40rem]">
+        <div>
+          <div className="relative max-w-xl border-neutral-smokeGrey ">
+            <ul
+              aria-label="date-setter"
+              className="uppercase font-poppins font-medium flex gap-5 "
+            >
+              <li className="flex flex-col">
+                <label htmlFor="day">Day</label>
+                <input
+                  type="text"
+                  className="w-32 h-14 border-solid border-[1px] rounded mt-1 border-neutral-offBlack outline-none p-2 font-bold text-2xl"
+                  placeholder="DD"
+                />
+              </li>
+              <li className="flex flex-col">
+                <label htmlFor="month">Month</label>
+                <input
+                  type="text"
+                  className="w-32 h-14 border-solid border-[1px] rounded my-1  border-neutral-offBlack outline-none p-2 font-bold text-2xl"
+                  placeholder="MM"
+                />
+              </li>
+              <li className="flex flex-col">
+                <label htmlFor="year">Year</label>
+                <input
+                  type="text"
+                  className="w-32 h-14 border-solid border-[1px] rounded mt-1  border-neutral-offBlack outline-none p-2 font-bold text-2xl"
+                  placeholder="YYYY"
+                />
+              </li>
+            </ul>
+
+            <div className="relative">
+              <button className="float-right">
+                <img
+                  src="/assets/images/icon-arrow.svg"
+                  className="bg-primary-purple p-4 rounded-full  "
+                  alt=""
+                />
+              </button>
+              <hr className=" absolute -bottom-10  border-neutral-smokeGrey w-[90%]   "/>
+            </div>
+
+
+          </div>
+
+        </div>
+
+        <div></div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily:{
+      poppins: ["Poppins", "sans-serif"]
+    },
+    colors:{
+      primary: {
+        purple: 'hsl(259, 100%, 65%)',
+        lightRed: 'hsl(0, 100%, 67%)'
+
+      },
+      neutral:{
+        white:'hsl(0, 0%, 100%)',
+        offWhite:'hsl(0, 0%, 94%)',
+        lightGrey: 'hsl(0, 0%, 86%)',
+        smokeGrey: 'hsl(0, 1%, 44%)',
+        offBlack: 'hsl(0, 0%, 8%)'
+      }
+    }
   },
   plugins: [],
 }
